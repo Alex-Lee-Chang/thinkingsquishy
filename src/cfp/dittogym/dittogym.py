3ffda4b03f24859c8deb283d5d61bc4548871cc9
Kev-Park <kevy0403@gmail.com>
2025-08-22 21:19:56 -0400

    update slurm job + shorten boundary scheduling

--- src/cfp/dittogym/dittogym.py
+++ src/cfp/dittogym/dittogym.py
@@ -483,7 +483,8 @@ class dittogym(gym.Env, ABC):
 
         points_np = self.x.to_numpy()
         # Radius of circle is 0.085; 0.065 provides a good min thickness
-        radius = 0.065 * (total_numsteps / 5000000)
+        schedule = 30000 # How fast will full boundary be achieved
+        radius = 0.065 * (total_numsteps / schedule)
 
         distances = np.linalg.norm(points_np - center, axis=1)
         filtered_indices = np.where(distances > radius)
