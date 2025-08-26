2628bcf1912d73ad7eb13e64a17835a7de0a52d6
Kev-Park <kevy0403@gmail.com>
2025-08-26 16:33:23 -0400

    update boundary

--- src/cfp/dittogym/dittogym.py
+++ src/cfp/dittogym/dittogym.py
@@ -484,7 +484,7 @@ class dittogym(gym.Env, ABC):
         points_np = self.x.to_numpy()
         # Radius of circle is 0.085; 0.065 provides a good min thickness
         schedule = 30000 # In how many steps will full boundary be achieved
-        radius = 0.055 * (total_numsteps / schedule)
+        radius = 0.045 * (total_numsteps / schedule)
 
         distances = np.linalg.norm(points_np - center, axis=1)
         filtered_indices = np.where(distances > radius)
